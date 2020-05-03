a36fbf3db3c4766dea14be18d722f76b54dc6629
Christine Bohnet <christine.bohnet@gmail.com>
2020-05-03 16:00:45 -0500

    week 1 edits

--- api-gateway/config.js
+++ api-gateway/config.js
@@ -1,3 +1,13 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Chris Bohnet
+; Date: 3 May 2020
+; Modified By:
+; Description: Gateway API
+============================================
+
+*/
 var config = {};
 
 config.web = {};
