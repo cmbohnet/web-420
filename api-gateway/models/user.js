3301ceb894e4f1ac9594884acd555cfb494e955f
Christine Bohnet <christine.bohnet@gmail.com>
2020-05-22 15:18:21 -0500

    week 4 assignments

--- api-gateway/models/user.js
+++ api-gateway/models/user.js
@@ -3,8 +3,9 @@
 ; Title: Assignment 2.3
 ; Author: Chris Bohnet
 ; Date: 3 May 2020
-; Modified By:
+; Modified By: Chris Bohnet
 ; Description: User model for Gateway API - responsible for creating User documents and querying database records
+; 5/21/20 - Add 2 new callback functions, add and getById
 ============================================
 
 */
@@ -20,6 +21,15 @@ var userSchema = new mongoose.Schema({
 
 module.exports = mongoose.model('User', userSchema);
 
+// user.save is used to add a new user in our database
+module.exports.add = (user, callback) => {
+    user.save(callback);
+}
+
+module.exports.getById = (id, callback) => {
+    var query = {_id: id};
+    User.findById(query, callback);
+}
 /**
  Database queries
  */
